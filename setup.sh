0bc121d6561f5d29120b4fc0bc7404909fc026ad
Chris Anderson <canderson30@unl.edu>
2015-07-02 22:40:48 -0500

    update

--- setup.sh
+++ setup.sh
@@ -5,38 +5,13 @@ if [ "$1" = "" ]; then
     exit 1
 fi
 
-wget http://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
-tar -zxvf Python-2.7.9.tgz
-cd Python-2.7.9
-cur="$PWD"
-./configure --prefix=$cur
-make
-make install
-cd ..
-
-wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-12.0.7.tar.gz
-tar -zxvf virtualenv-12.0.7.tar.gz
-Python-2.7.9/bin/python2.7 virtualenv-12.0.7/virtualenv.py rumenEnv
-source rumenEnv/bin/activate
-wget https://pypi.python.org/packages/source/n/numpy/numpy-1.9.2.tar.gz
-wget https://pypi.python.org/packages/source/q/qiime/qiime-1.9.1.tar.gz
-pip install numpy-1.9.2.tar.gz
-pip install qiime-1.9.1.tar.gz
-
-#For Mac, take out following three lines 
-#if want to install linux that has gfortran already
-wget http://coudert.name/software/gfortran-4.8.2-Mavericks.dmg
-hdiutil attach gfortran-4.8.2-Mavericks.dmg
-sudo installer -pkg /Volumes/gfortran-4.8.2-Mavericks/gfortran-4.8.2-Mavericks/gfortran.pkg -target /
-
-cd rumenEnv/bin
-cur="$PWD"
-cd ..
-wget http://rweb.quant.ku.edu/cran/src/base/R-3/R-3.2.0.tar.gz  
-tar -zxvf R-3.2.0.tar.gz
-cd R-3.2.0
-./configure --prefix=$cur
-make
+wget https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh
+tar -zxvf conda-3.7.0.tar.gz
+bash Miniconda-latest-MacOSX-x86_64.sh
+miniconda/bin/conda create -n rumenEnv python pip numpy matplotlib IPython future natsort scipy pandas scikit-bio gdata
+source miniconda/bin/activate rumenEnv
+pip install https://github.com/biocore/qiime/archive/1.9.0.tar.gz
+miniconda/bin/conda install -c r r
 
 wget -O bin/usearch $1
 chmod 775 bin/usearch
@@ -55,6 +30,3 @@ mv mothur/mothur bin/
 
 wget https://raw.githubusercontent.com/chrisLanderson/rumen_adaptation/master/rumen_adaptation.Rmd
 
-rm R-3.2.0.tar.gz
-rm Mothur.mac_64.OSX-10.9.zip
-rm -rf fastx
