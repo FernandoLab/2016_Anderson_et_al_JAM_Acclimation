fddd12fdeaa8fc0b84685bb9cdc08d3587c9cad8
Chris Anderson <canderson30@unl.edu>
2015-07-02 22:50:27 -0500

    update

--- setup.sh
+++ setup.sh
@@ -8,13 +8,13 @@ fi
 wget https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh
 tar -zxvf conda-3.7.0.tar.gz
 bash Miniconda-latest-MacOSX-x86_64.sh
-miniconda/bin/conda create -n rumenEnv python pip numpy matplotlib IPython future natsort scipy pandas scikit-bio gdata
+miniconda/bin/conda create -n rumenEnv python pip numpy matplotlib scipy pandas
 source miniconda/bin/activate rumenEnv
 pip install https://github.com/biocore/qiime/archive/1.9.0.tar.gz
 miniconda/bin/conda install -c r r
 
-wget -O bin/usearch $1
-chmod 775 bin/usearch
+wget -O miniconda/envs/rumenEnv/bin/usearch $1
+chmod 775 miniconda/envs/rumenEnv/bin/usearch
 
 mkdir fastx
 cd fastx
@@ -22,11 +22,13 @@ wget http://hannonlab.cshl.edu/fastx_toolkit/fastx_toolkit_0.0.13_binaries_MacOS
 bzip2 -d fastx_toolkit_0.0.13_binaries_MacOSX.10.5.8_i386.tar.bz2
 tar -xvf fastx_toolkit_0.0.13_binaries_MacOSX.10.5.8_i386.tar
 cd ..
-mv fastx/bin/* bin/
+mv fastx/bin/* miniconda/envs/rumenEnv/bin/
+rm -rf fastx
 
 wget https://github.com/mothur/mothur/releases/download/v1.35.1/Mothur.mac_64.OSX-10.9.zip
 unzip Mothur.mac_64.OSX-10.9.zip
-mv mothur/mothur bin/
+mv mothur/mothur miniconda/envs/rumenEnv/bin/
+rm Mothur.mac_64.OSX-10.9.zip
 
 wget https://raw.githubusercontent.com/chrisLanderson/rumen_adaptation/master/rumen_adaptation.Rmd
 
