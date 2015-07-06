d777ebcd45f77fc39d7262031f2d92ee1e1c0def
Chris Anderson <canderson30@unl.edu>
2015-07-06 16:42:01 -0500

    update

--- setup.sh
+++ setup.sh
@@ -5,16 +5,16 @@ if [ "$1" = "" ]; then
     exit 1
 fi
 
-wget https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh
-tar -zxvf conda-3.7.0.tar.gz
-bash Miniconda-latest-MacOSX-x86_64.sh
-miniconda/bin/conda create -n rumenEnv python pip numpy matplotlib scipy pandas
-source miniconda/bin/activate rumenEnv
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-MacOSX-x86_64.sh
+bash Anaconda-2.3.0-MacOSX-x86_64.sh
+anaconda/bin/conda create -n rumenEnv python pip numpy matplotlib scipy pandas
+source anaconda/bin/activate rumenEnv
 pip install https://github.com/biocore/qiime/archive/1.9.0.tar.gz
-miniconda/bin/conda install -c r r
+anaconda/bin/conda install -c r r r-xml r-ggplot2 r-matrix r-knitr r-reshape
 
-wget -O miniconda/envs/rumenEnv/bin/usearch $1
-chmod 775 miniconda/envs/rumenEnv/bin/usearch
+
+wget -O anaconda/envs/rumenEnv/bin/usearch $1
+chmod 775 anaconda/envs/rumenEnv/bin/usearch
 
 mkdir fastx
 cd fastx
@@ -22,12 +22,12 @@ wget http://hannonlab.cshl.edu/fastx_toolkit/fastx_toolkit_0.0.13_binaries_MacOS
 bzip2 -d fastx_toolkit_0.0.13_binaries_MacOSX.10.5.8_i386.tar.bz2
 tar -xvf fastx_toolkit_0.0.13_binaries_MacOSX.10.5.8_i386.tar
 cd ..
-mv fastx/bin/* miniconda/envs/rumenEnv/bin/
+mv fastx/bin/* anaconda/envs/rumenEnv/bin/
 rm -rf fastx
 
 wget https://github.com/mothur/mothur/releases/download/v1.35.1/Mothur.mac_64.OSX-10.9.zip
 unzip Mothur.mac_64.OSX-10.9.zip
-mv mothur/mothur miniconda/envs/rumenEnv/bin/
+mv mothur/mothur anaconda/envs/rumenEnv/bin/
 rm Mothur.mac_64.OSX-10.9.zip
 
 wget https://raw.githubusercontent.com/chrisLanderson/rumen_adaptation/master/rumen_adaptation.Rmd
